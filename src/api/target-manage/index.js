3b75a14b6af5267ec7066062aba4fd65b45efd21
chenyinhuan <yinhuan.chen@51hgp.com>
2021-06-24 17:56:22 +0800

    增加指标接口

--- src/api/target-manage/index.js
+++ src/api/target-manage/index.js
@@ -16,3 +16,21 @@ export const getTargeList = (params) => api.get(`/task-admin/task/targe/page`, {
  * @params params
  */
 export const getTargeListExtend = (params) => api.post(`/task-admin/task/targe/condtion/list`, params);
+
+/**
+ * 删除指标
+ * @params params
+ */
+export const deleteTarget = (params) => api.post(`/task-admin/task/targe/del/${params.id}`, params);
+
+/**
+ * 通过id获取某条指标详情
+ * @params params
+ */
+export const getTargeDetail = (params) => api.post(`/task-admin/task/targe/detail/${params.id}`, params);
+
+/**
+ * 更新指标
+ * @params params
+ */
+export const updateTarget = (params) => api.post(`/task-admin/task/targe/updateTarge`, params);
