145a9464881932eef409340488ff6aa1435c23af
wulanlan <2394456570@qq.com>
2021-06-26 22:34:59 +0800

    衍生字段 字段校验

--- src/views/field-manage/group/nativeField.vue
+++ src/views/field-manage/group/nativeField.vue
@@ -33,7 +33,7 @@
           <el-option v-for="item in dataTypeList" :key="item.value" :label="item.label" :value="item.value">
           </el-option>
         </el-select>
-        <span class="validate-info" style="color: #FF8C00;" v-if="showValidate && form.formType != '' &&form.dataType==''&& form.formType != 4">请选择数据类型</span>
+        <span class="validate-info" style="color: #FF8C00;" v-if="showValidate && !form.dataType&& form.formType != 4">请选择数据类型</span>
       </div>
       <div class="item">
         <p v-if="form.formType == 2 || form.formType == 3">枚举值</p>
@@ -136,9 +136,12 @@
         ]
       },
       save() {
-        if (this.form.fieldName == '' || this.form.name == '' || this.form.formType == '' || this.checkFieldName || this.checkName) return this.showValidate = true;
-        for(let i in this.enums){
-          if(!this.enums[i].enumValue) return this.showValidate = true;
+        console.log(this.form)
+        if (this.form.fieldName == '' || this.form.name == '' || !this.form.dataType || this.form.formType == '' || this.checkFieldName || this.checkName) return this.showValidate = true;
+        if(this.form.formType == 2 || this.form.formType == 3){
+          for(let i in this.enums){
+            if(!this.enums[i].enumValue) return this.showValidate = true;
+          }
         }
         let params = {
           "dataType": this.form.dataType, //数据类型 1：字符串型string，2：整数int，3小数数值float，4日期date，5 时间time
